afa4a5a50805fddfd217a68a4f58b1f14b88337d
Aditya Hajare <aditya43@gmail.com>
2021-10-06 20:02:57 +0530

    Deploy network

---
+++ 02-Setup-Network/03-Deploy-Network.sh
@@ -0,0 +1,12 @@
+#######################
+## start the network ##
+#######################
+
+# move to the docker folder
+cd ~/mount/network/docker
+
+# start the containers from the compose file
+docker-compose -f docker-compose-test-net.yaml up -d
+
+# checkout the network
+docker ps
