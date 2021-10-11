3db8429f0fcd3a4e9434482098793d714592548a
Aditya Hajare <aditya43@gmail.com>
2021-10-11 20:23:47 +0530

    Enroll admin blue print

---
+++ 05-Application/application/enrollAdmin.js
@@ -0,0 +1,18 @@
+const FabricCAServices = require('fabric-ca-client');
+const {Wallets} = require('fabric-network');
+
+const fs = require('fs');
+const path = require('path');
+
+async function main() {
+    try {
+        // Load the network configuration
+        // Create the CA client for interacting with the CA
+        // Create a new file system based wallet for managing identities
+        // Enroll the admin user and import the new identity into the wallet
+    } catch (e) {
+
+    }
+}
+
+main();
