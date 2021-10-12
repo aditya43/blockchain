6dd92991214d5466246065951740dcaa4a094ae9
Aditya Hajare <aditya43@gmail.com>
2021-10-12 15:26:10 +0530

    Remove fabcar chaincode

--- 03-Fabcar/network/scripts/networkdown.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd ~/mount/network/docker
-docker-compose -f ./docker-compose-test-net.yaml down
-docker-compose -f ./docker-compose-ca.yaml down
-docker volume prune -f
-popd
--- 03-Fabcar/network/scripts/networkup.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-pushd ~/mount/network/docker
-
-docker-compose -f ./docker-compose-test-net.yaml up -d 
-
-popd
