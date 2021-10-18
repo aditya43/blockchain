3f2b0cd1acaf478f8b40d25b258e40bdc9681bba
Aditya Hajare <aditya43@gmail.com>
2021-10-18 18:30:13 +0530

    Set env vars

---
+++ 09-Dev-Env/network/bin/env.sh
@@ -0,0 +1,8 @@
+# Environment in use
+export CORE_PEER_LOCALMSPID=AcmeMSP
+export CORE_PEER_MSPCONFIGPATH=/vagrant/network/crypto/crypto-config/peerOrganizations/acme.com/users/Admin@acme.com/msp
+export CORE_PEER_ID=acme-peer1.acme.com
+export FABRIC_LOGGING_SPEC=info
+export CORE_PEER_ADDRESS=acme-peer1.acme.com:7051
+export ORDERER_ADDRESS=orderer.acme.com:7050
+export FABRIC_CFG_PATH=/vagrant/network/config
