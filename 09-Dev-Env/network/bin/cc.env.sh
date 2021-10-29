1122275784a8b11333cdcf58b427a2ce89d283dc
Aditya Hajare <aditya43@gmail.com>
2021-10-29 19:25:11 +0530

    Chaincode env

---
+++ 09-Dev-Env/network/bin/cc.env.sh
@@ -0,0 +1,22 @@
+# Generated: Wed Oct 20 13:13:57 UTC 2021
+export CC_LANGUAGE=golang
+export CC_PATH=token/v2
+export CC_NAME=token
+export CC_VERSION=1.0
+export CC_CHANNEL_ID=airlinechannel
+export CC_CONSTRUCTOR='{"Args":["init"]}'
+export CC_QUERY_ARGS='{"Args":["query","b"]}'
+export CC_INVOKE_ARGS='{"Args":["invoke","a","b","5"]}'
+export CORE_CHAINCODE_ID_NAME='token'
+export CORE_CHAINCODE_LOGGING_LEVEL=''
+export CORE_CHAINCODE_LOGGING_SHIM=''
+export CC_PRIVATE_DATA_JSON=''
+export CC_ENDORSEMENT_POLICY=""
+export CC2_SEQUENCE="1"
+export CC2_INIT_REQUIRED="true"
+export CC2_PACKAGE_FOLDER="/home/vagrant/packages"
+export CC2_SIGNATURE_POLICY=""
+export CC2_CHANNEL_CONFIG_POLICY=""
+export CC2_ENDORSING_PEERS=""
+export CC2_ENV_FOLDER="/vagrant/network/bin/env"
+export INTERNAL_DEV_VERSION="1"
