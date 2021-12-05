6efb15f87106769f1823143fa8556e35530be3b6
Aditya Hajare <aditya43@gmail.com>
2021-12-05 19:25:49 +0530

    Network shutdown

--- 09-Dev-Env/network/bin/_shutdown.sh
+++ 09-Dev-Env/network/bin/_shutdown.sh
@@ -1,4 +1,4 @@
 #PEER_MODE=net
 #Command=dev-init.sh -s 
-#Generated: Fri Oct 15 12:25:06 UTC 2021 
+#Generated: Sat Nov  6 10:55:00 UTC 2021 
 docker-compose  -f ./compose/docker-compose.base.yaml     -f ./compose/docker-compose.couchdb.yaml     down 
